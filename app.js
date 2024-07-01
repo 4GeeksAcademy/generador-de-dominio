20fe8250b15edeaad4029a6fdb06cb0953d7b154
victoriamartinez23 <victoriamartinezruiz23@gmail.com>
2024-07-01 13:51:59 +0000

    Generador de dominios

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronombre = ['el', 'un'];
+let adj = ['gran', 'genial'];
+let nombre = ['mundo', 'universo'];
+let dominio = ['net', 'es', 'us', 'io'];
+
+     for (j=0; j<pronombre.length; j++) {
+        for (i=0; i<pronombre.length; i++) {
+            for (k=0; k<nombre.length; k++) {
+                for (h=0; h<dominio.length; h++) {
+                    console.log(pronombre[i]+adj[j]+nombre[k]+"."+dominio[h]);
+}
+}
+}
+}
